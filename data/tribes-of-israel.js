565205d30329b243452f6e3cff465e0d6822c363
johnalexguerrero <johnalexguerrero@gmail.com>
2024-10-22 17:40:20 -0500

    append son to Israel

--- data/tribes-of-israel.js
+++ data/tribes-of-israel.js
@@ -1,5 +1,5 @@
 const data = {
-    "name": "Israel",
+    "name": "Hijos de Israel",
     "children": [
       {
         "name": "Asber",
@@ -59,6 +59,50 @@ const data = {
             {
                 "name": "Efrain", 
                 "children": [
+                  {
+                    "name":"Zuf",
+                    "value":100,
+                    "versos":['1 S 1. 1'],
+                    "children":[
+                      {
+                        "name":"Tohu",
+                        "value":100,
+                        "versos":['1 S 1. 1'],
+                        "children":[
+                          {
+                            "name":"Eliú",
+                            "value":100,
+                            "versos":['1 S 1. 1'],
+                            "children":[
+                              {
+                                "name":"Jeroham",
+                                "value":100,
+                                "versos":['1 S 1. 1'],
+                                "children":[
+                                  {
+                                    "name":"Elcana",
+                                    "value":100,
+                                    "versos":['1 S 1. 1'],
+                                    "children":[
+                                      {
+                                        "name":"Samuel",
+                                        "value":80,
+                                        "versos":['1 S 1. 20']
+                                      }
+                                    ]
+                                  },
+            
+                                ]
+                              },
+
+                            ]
+                          },
+
+                        ]
+                      },
+
+                    ]
+                  },
                     {
                         "name":"Micaía", 
                         "value": 60, 
@@ -74,27 +118,78 @@ const data = {
         "name": "Judah",
         "children": [
           {
-            "name": "Elimelec", 
-            "value": 10,
-            "versos":["Rt 1. 1"],
+            "name":"Fares",
+            "value":100,
+            "versos":['Gn 38. 27-30'],
             "children":[
-                {
-                    "name":"Mahlón",
-                    "value":100,
-                },
-                {
-                    "name":"Quelión",
-                    "value":100,
-                },
+              {
+                "name":"Hezrón","value":80,"versos":['Rt 4. 18'],
+                "children":[
+                  {
+                    "name":"Ram","value":80,"versos":['Rt 4. 18'],
+                    "children":[
+                      {
+                        "name":"Aminadab","value":80,"versos":['Rt 4. 18'],
+                        "children":[
+                          {
+                            "name":"Naasón","value":80,"versos":['Rt 4. 18'],
+                            "children":[
+                              {
+                                "name":"Salmón","value":80,"versos":['Rt 4. 18'],
+                                "children":[
+                                  {
+                                    "name": "Elimelec", 
+                                    "value": 10,
+                                    "versos":["Rt 1. 1"],
+                                    "children":[
+                                        {
+                                          "name":"Mahlón",
+                                          "value":100,
+                                          "versos":['Rt 4. 8-10'],
+                                        },
+                                        {
+                                          "name":"Quelión",
+                                          "value":100,
+                                        },
+                                      ]
+                                    },
+                                    {
+                                      "name": "booz", 
+                                      "value": 160,
+                                      "versos":["Rt 2. 1-23"],
+                                      "children":[
+                                        {
+                                          "name":"Obed", "value":80,"versos":['Rt 4. 17'],
+                                          "children":[
+                                            {
+                                              "name":'Isaí',"value":90,"versos":['Rt 4. 17'], 
+                                              "children":[
+                                                {"name": "David", "value": 100, "versos":["Rt 4. 17"]}
+                                              ]
+                                            }
+                                          ]
+                                        }
+                                      ]
+                                },
+                                  {"name": "fulano", "value": 80, "versos":["Rt 4. 1"]},
+                                  
+                                ]
+                              },
+                            ]
+                          },
+                        ]
+                      },
+                    ]
+                  },
+                ]
+              },
             ]
-        },
-        {
-            "name": "booz", 
-            "value": 160,
-            "versos":["Rt 2. 1-23"]
-        },
-          {"name": "C2", "value": 80},
-          {"name": "C3", "value": 250}
+          },
+          {
+            "name":"Zara",
+            "value":80,
+            "versos":['Gn 38. 27-30']
+          },
         ]
       },
       {
